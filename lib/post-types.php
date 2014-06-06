17e860b581b6d4b886c9a9f89e62282e9ca214c5
Corey Maass <corey@gelform.com>
2014-06-06 14:54:15 -0500

    adding loop for easy cpt creation

--- lib/post-types.php
+++ lib/post-types.php
@@ -8,6 +8,11 @@
 // Register Custom Post Type
 function custom_post_types() 
 {
+	/**
+	 * For individual, custom cpt's
+	 * Copy and paste this and changes variables as needed
+	 */
+
 	// $labels = array(
 	// 	'name'                => _x( 'Items', 'Post Type General Name', 'text_domain' ),
 	// 	'singular_name'       => _x( 'Item', 'Post Type Singular Name', 'text_domain' ),
@@ -45,6 +50,63 @@ function custom_post_types()
 	// );
 	// register_post_type( 'custom_items', $args );
 
+
+
+	/**
+	 * To create multilpe cpt's, quick and dirty
+	 * Add more arrays to this array, and change the variables as needed. 
+	 * Then the loop will create all of them
+	 */
+	// $post_types = array(
+	// 	array(
+	// 		'proper_plural' => 'Shows',
+	// 		'proper_singular' => 'Show',
+	// 		'post_type_variable_name' => 'custom_shows',
+	// 		'slug' => 'show'
+	// 	)
+	// );
+
+	// foreach ($post_types as $post_type)
+	// {
+	// 	$post_type = (object) $post_type;
+	// 	$labels = array(
+	// 		'name'                => $post_type->proper_plural, 'Post Type General Name',
+	// 		'singular_name'       => $post_type->proper_singular, 'Post Type Singular Name',
+	// 		'menu_name'           => $post_type->proper_plural,
+	// 		'parent_item_colon'   => 'Parent ' . $post_type->proper_singular . ':',
+	// 		'all_items'           => 'All ' . $post_type->proper_plural,
+	// 		'view_item'           => 'View ' . $post_type->proper_singular,
+	// 		'add_new_item'        => 'Add New ' . $post_type->proper_singular,
+	// 		'add_new'             => 'Add New',
+	// 		'edit_item'           => 'Edit ' . $post_type->proper_singular,
+	// 		'update_item'         => 'Update ' . $post_type->proper_singular,
+	// 		'search_items'        => 'Search ' . $post_type->proper_singular,
+	// 		'not_found'           => 'Not found',
+	// 		'not_found_in_trash'  => 'Not found in Trash',
+	// 	);
+
+	// 	$args = array(
+	// 		'label'               => $post_type->post_type_variable_name,
+	// 		'description'         => 'Single ' . $post_type->proper_singular . ' type',
+	// 		'labels'              => $labels,
+	// 		'supports'            => array(),
+	// 		'hierarchical'        => false,
+	// 		'public'              => true,
+	// 		'show_ui'             => true,
+	// 		'show_in_menu'        => true,
+	// 		'show_in_nav_menus'   => true,
+	// 		'show_in_admin_bar'   => false,
+	// 		'menu_icon'           => '',
+	// 		'can_export'          => true,
+	// 		'has_archive'         => false,
+	// 		'exclude_from_search' => false,
+	// 		'publicly_queryable'  => true,
+	// 		'capability_type'     => 'page',
+	// 		'rewrite' => array('slug' => $post_type->slug)
+	// 	);
+
+	// 	register_post_type( $post_type->post_type_variable_name, $args );
+	// }
 
 
 } // custom_post_types
